5a855a0230cd860b2e299dc352f00b6ae3d0daf7
mja1337 <76176518+mja1337@users.noreply.github.com>
2023-10-13 08:00:38 +0100

    Update scripts.js
    tidied up

--- scripts.js
+++ scripts.js
@@ -1,27 +1,61 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-  loadFromLocalStorage();
-  renderPlayers();
+    setupLogoInput();
+    setupPlayerSelect();
+    setupLogoDoubleClick();
+    renderPlayers();
 });
 
-function loadFromLocalStorage() {
+function setupLogoDoubleClick() {
+    const homeLogo = document.querySelector('.home-team .team-logo');
+    const playerSelect = document.querySelector('#home-player-select');
+    const refreshButton = document.querySelector('#refresh-button');
 
-  populateTeamList('.home-player-list', storedHomeTeam);
-  populateTeamList('.away-player-list', storedAwayTeam);
+    playerSelect.style.display = 'none';
+    refreshButton.style.display = 'none';
+
+    homeLogo.addEventListener('dblclick', function() {
+        playerSelect.style.display = (playerSelect.style.display === 'none') ? 'block' : 'none';
+        refreshButton.style.display = (refreshButton.style.display === 'none') ? 'block' : 'none';
+    });
+
+    refreshButton.addEventListener('click', function() {
+        location.reload();
+    });
 }
 
-function populateTeamList(selector, playerData) {
-  const teamList = document.querySelector(selector);
-  teamList.innerHTML = "";
+function setupPlayerSelect() {
+    const playerSelect = document.querySelector('#home-player-select');
+    const playerList = document.querySelector('.home-player-list');
 
-  playerData.forEach(player => {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${player.number}. ${player.name}`;
-      listItem.contentEditable = "true";
-      teamList.appendChild(listItem);
-  });
+    playerSelect.addEventListener('change', function() {
+        for (let li of playerList.querySelectorAll('li')) {
+            const textContent = li.textContent.trim();
+            if (textContent.match(/^\d+\.\s*$/)) {
+                li.textContent = textContent + ' ' + playerSelect.value;
+                break;
+            }
+        }
+        playerSelect.value = "";
+        playerSelect.style.display = 'none';
+    });
 }
 
+function setupLogoInput() {
+    const logoInput = document.querySelector('#awayTeamLogoUrl');
+    const logoImg = document.querySelector('#awayTeamLogoImg');
+
+    logoInput.addEventListener('change', function() {
+        if (logoInput.value) {
+            logoImg.src = logoInput.value;
+            logoImg.onload = function() {
+                logoInput.style.display = 'none';
+            };
+        } else {
+            logoInput.style.display = 'block';
+            logoImg.src = '';
+        }
+    });
+}
 function renderPlayers() {
 
   homeTeamNames.forEach((name, index) => {
@@ -68,94 +102,3 @@ function renderPlayers() {
           name: parts[1]
       };
   });
-
-document.addEventListener("DOMContentLoaded", function () {
-  setupLogoInput();
-});
-
-function setupLogoInput() {
-  const logoInput = document.querySelector('#awayTeamLogoUrl');
-  const logoImg = document.querySelector('#awayTeamLogoImg');
-
-  logoInput.addEventListener('change', function() {
-      if (logoInput.value) {
-          logoImg.src = logoInput.value;
-          logoImg.onload = function() {
-              logoInput.style.display = 'none';
-          };
-      } else {
-          logoInput.style.display = 'block';
-          logoImg.src = '';
-      }
-  });
-}
-
-//document.addEventListener("DOMContentLoaded", function () {
-//  const zoomOutButton = document.getElementById('zoom-out-button');
-//  let scale = 1;
-//  zoomOutButton.addEventListener('click', function() {
-//    scale -= 0.1; // decrement scale by 10% on each click
-//    document.body.style.transform = `scale(${scale})`;
-//  });
-//});
-
-document.addEventListener("DOMContentLoaded", function () {
-    setupLogoInput();
-    setupPlayerSelect();
-    setupLogoDoubleClick();
-});
-
-function setupLogoDoubleClick() {
-    const homeLogo = document.querySelector('.home-team .team-logo');
-    const playerSelect = document.querySelector('#home-player-select');
-
-    // Initially hide the select dropdown
-    playerSelect.style.display = 'none';
-
-    homeLogo.addEventListener('dblclick', function() {
-        // Toggle visibility of the select dropdown on double click of the home logo
-        playerSelect.style.display = (playerSelect.style.display === 'none') ? 'block' : 'none';
-    });
-}
-
-function setupPlayerSelect() {
-    const playerSelect = document.querySelector('#home-player-select');
-    const playerList = document.querySelector('.home-player-list');
-
-    playerSelect.addEventListener('change', function() {
-        for (let li of playerList.querySelectorAll('li')) {
-            const textContent = li.textContent.trim();
-            if (textContent.match(/^\d+\.\s*$/)) {
-                li.textContent = textContent + ' ' + playerSelect.value;
-                break;
-            }
-        }
-        // Reset the select element to the default option and hide it
-        playerSelect.value = "";
-        playerSelect.style.display = 'none';
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    setupLogoInput();
-    setupPlayerSelect();
-});
-
-function setupPlayerSelect() {
-    const playerSelect = document.querySelector('#home-player-select');
-    const playerList = document.querySelector('.home-player-list');
-
-    playerSelect.addEventListener('change', function() {
-        // Find the next available slot in the player list
-        for (let li of playerList.querySelectorAll('li')) {
-            const textContent = li.textContent.trim();
-            // If the list item is empty (only contains a number and a dot), insert the player name
-            if (textContent.match(/^\d+\.\s*$/)) {
-                li.textContent = textContent + ' ' + playerSelect.value;
-                break; // Exit the loop once the player name is inserted
-            }
-        }
-        // Reset the select element to the default option
-        playerSelect.value = "";
-    });
-}
